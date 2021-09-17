4bba4ff234d40aa4325870926beea3d73407d517
cjwishon <73266422+cjwishon@users.noreply.github.com>
2021-09-16 20:33:59 -0600

    demo change

--- assets/js/demo.js
+++ assets/js/demo.js
@@ -21,6 +21,7 @@ function queryLeague() {
     var yearElement = document.getElementById("year");
     year = yearElement.value;
 
+    console.log("HELLO 1");
     //document.cookie = "swid=" + swid;
     console.log(document.cookie);
     document.cookie = "espn_s2=" + espn_s2;
