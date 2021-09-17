83bb9c6d48a712b98ae088b8770b4f4b2511957f
cjwishon <73266422+cjwishon@users.noreply.github.com>
2021-09-16 20:32:48 -0600

    test

--- assets/js/demo.js
+++ assets/js/demo.js
@@ -21,7 +21,8 @@ function queryLeague() {
     var yearElement = document.getElementById("year");
     year = yearElement.value;
 
-    document.cookie = "swid=" + swid;
+    //document.cookie = "swid=" + swid;
+    console.log(document.cookie);
     document.cookie = "espn_s2=" + espn_s2;
     console.log(document.cookie);
 
